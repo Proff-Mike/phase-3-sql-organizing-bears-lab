f7e4621aa9514fa79729e1bc8dcf0b1cf5539a4f
Michael Okoe <michael.okoejr@gmail.com>
2022-09-07 11:52:07 +0000

    commit tree 795e1ad69a5d3769a9a38bf670e4023d734a8e64

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 2, "M", "Brown", "Wild", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 4, "M", "White", "Calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 6, "F", "Black", "Calm", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Seagent Brown", 6, "M", "Black", "Wild", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 2, "F", "Brown", "Wild", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 3, "F", "Ash", "Wild", 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "Brown", "Wild", 0);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 2, "M", "Ash", "Wild", 0);
